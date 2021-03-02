15dad036ee2869b50ad2b89a6f4692f629052522
patryk <sdark431@gmail.com>
2021-03-02 23:51:27 +0100

    Dodanie mechaniki poruszania sie

---
+++ Assets/Scripte/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    GameObject body;
+    Animator myAnimator;
+
+
+    private Vector2 _move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = transform.Find("Body").gameObject;
+        myAnimator = body.GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if(_move.x == 0) myAnimator.SetBool("isRunning", false);
+
+        transform.position += new Vector3(_move.x * speed * Time.deltaTime, 0f);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        myAnimator.SetBool("isRunning", true);
+        _move = context.ReadValue<Vector2>();
+        Debug.Log($"Right move: { _move }");
+    }
+    
+}
