d3a140184a5d951339429f957e457d7a6c437af6
patryk <sdark431@gmail.com>
2021-03-02 23:57:40 +0100

    Dodanie obracania sie postaci podczas chodzenia

--- Assets/Scripte/Player.cs
+++ Assets/Scripte/Player.cs
@@ -24,16 +24,27 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if(_move.x == 0) myAnimator.SetBool("isRunning", false);
+        ChangeAnimation();
+        ChangeOrintation();
 
         transform.position += new Vector3(_move.x * speed * Time.deltaTime, 0f);
     }
 
+    private void ChangeOrintation()
+    {
+        if (_move.x < 0) transform.localScale = new Vector3(-1f, 1f);
+        if (_move.x > 0) transform.localScale = new Vector3( 1f, 1f);
+    }
+
+    private void ChangeAnimation()
+    {
+        if (_move.x == 0) myAnimator.SetBool("isRunning", false);
+        else myAnimator.SetBool("isRunning", true);
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
-        myAnimator.SetBool("isRunning", true);
         _move = context.ReadValue<Vector2>();
-        Debug.Log($"Right move: { _move }");
     }
     
 }
