d4bacd28a7783f2fbe36b740ac9fb870f5293420
patryk <sdark431@gmail.com>
2021-03-03 02:45:34 +0100

    Implementacja podwujnego skoku

--- Assets/Scripte/Player.cs
+++ Assets/Scripte/Player.cs
@@ -10,22 +10,34 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 5f;
 
-    Animator myAnimator;
+    [Header("Jumping")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float checkRadius;
+    [SerializeField] LayerMask whatIsGround;
+    [SerializeField] int extraJump;
+
+    Animator _Animator;
     Rigidbody2D _rigidbody2D;
 
+    const float CALLBACK_COUNT = 2f;
+    float _move;
+    int _extraJump;
+    private bool isGrounded;
 
-    private float _move;
 
     // Start is called before the first frame update
     void Start()
     {
-        myAnimator = GetComponent<Animator>();
+        _extraJump = 0;
+        _Animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
     }
 
     private void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
         ChangeAnimation();
         FlipSprite();
         _rigidbody2D.velocity = new Vector3(_move * speed, _rigidbody2D.velocity.y);
@@ -34,8 +46,8 @@ public class Player : MonoBehaviour
 
     private void ChangeAnimation()
     {
-        if (_move == 0) myAnimator.SetBool("isRunning", false);
-        else myAnimator.SetBool("isRunning", true);
+        if (_move == 0) _Animator.SetBool("isRunning", false);
+        else _Animator.SetBool("isRunning", true);
     }
 
     private void FlipSprite()
@@ -49,13 +61,24 @@ public class Player : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         _move = context.ReadValue<float>();
-        Debug.Log(_move);
     }
     
     public void OnJump(InputAction.CallbackContext context)
     {
-        _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
-        Debug.Log(_move);
+        if (isGrounded) { _extraJump = 0; }
+
+        if ((_extraJump+1) / CALLBACK_COUNT <= 1)
+        {
+            Debug.Log($"jump { _extraJump }");
+            _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
+            _extraJump++;
+        }
+        else if (_extraJump < extraJump * CALLBACK_COUNT)
+        {
+            Debug.Log("Another jump}");
+            _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
+            _extraJump++;
+        }
     }
     
 }
