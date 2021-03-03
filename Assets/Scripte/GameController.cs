062c3d63bb6e6f79641d623d157c52a47f619ade
patryk <sdark431@gmail.com>
2021-03-03 14:12:49 +0100

    Kamera teraz sledzi gracza

---
+++ Assets/Scripte/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] public bool doubleJumpEvable;
+    [SerializeField] public bool stompEvable;
+    [SerializeField] Transform playerTrasform;
+    public CameraFollow cameraFollow;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollow.Setup(() => playerTrasform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripte/Player.cs
+++ Assets/Scripte/Player.cs
@@ -7,41 +7,61 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 5f;
+    [Serializable]
+    public class Jumping 
+    {
+        public float jumpForce = 5f;
+        public float stompForce = 5f;
+        public Transform groundCheck;
+        public Transform wallCheck;
+        public float checkRadius;
+        public LayerMask whatIsGround;
+        public int extraJump;
+        public GameObject jumpEffect;
+        public float effectLiveTime = 0.3f;
+    }
 
-    [Header("Jumping")]
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float checkRadius;
-    [SerializeField] LayerMask whatIsGround;
-    [SerializeField] int extraJump;
-    [SerializeField] GameObject jumpEffect;
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] Jumping jumping;
+
 
     Animator _Animator;
     Rigidbody2D _rigidbody2D;
+    GameController _gameController;
 
-    const float CALLBACK_COUNT = 2f;
+    const float CALLBACK_COUNT = 3f;
     float _move;
     int _extraJump;
-    private bool isGrounded;
+    bool isGrounded;
+    bool isWall;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        _extraJump = 0;
+        _extraJump = 1;
         _Animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _gameController = FindObjectOfType<GameController>();
 
     }
 
     private void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(jumping.groundCheck.position, jumping.checkRadius, jumping.whatIsGround);
+        isWall = Physics2D.OverlapCircle(jumping.wallCheck.position, jumping.checkRadius, jumping.whatIsGround);
 
         ChangeAnimation();
         FlipSprite();
-        _rigidbody2D.velocity = new Vector3(_move * speed, _rigidbody2D.velocity.y);
+        if (!isWall)
+        {
+            _rigidbody2D.velocity = new Vector3(_move * speed, _rigidbody2D.velocity.y);
+        }
+        else
+        {
+            _rigidbody2D.velocity = new Vector3(0f, _rigidbody2D.velocity.y);
+        }
 
     }
 
@@ -61,25 +81,50 @@ public class Player : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        _move = context.ReadValue<float>();
+        if (!isWall)
+        {
+            _move = context.ReadValue<float>();
+        }
+        else if(context.ReadValue<float>() < 0)
+        {
+            _move = context.ReadValue<float>();
+        }
+        else
+        {
+            _move = 0f;
+        }
     }
     
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (isGrounded) { _extraJump = 0; }
+        if (isGrounded) { _extraJump = 1; }
 
         if ((_extraJump+1) / CALLBACK_COUNT <= 1)
         {
-            _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
-            _extraJump++;
+            Debug.Log("jump");
+            Jump(context);
         }
-        else if (_extraJump < extraJump * CALLBACK_COUNT)
+        else if (_extraJump < jumping.extraJump * CALLBACK_COUNT && _gameController.doubleJumpEvable)
         {
-            _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
-            _extraJump++;
-            var effects = Instantiate(jumpEffect, transform.position, Quaternion.identity);
-            Destroy(effects, 0.3f);
+            Debug.Log("Double jump");
+            Jump(context);
+            var effects = Instantiate(jumping.jumpEffect, transform.position, Quaternion.identity);
+            Destroy(effects, jumping.effectLiveTime);
         }
     }
-    
+
+    public void OnStomp(InputAction.CallbackContext context)
+    {
+        if (!isGrounded && _gameController.stompEvable)
+        {
+            Debug.Log("Stomp");
+            _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, -context.ReadValue<float>() * jumping.stompForce);
+        }
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumping.jumpForce);
+        _extraJump++;
+    }
 }
