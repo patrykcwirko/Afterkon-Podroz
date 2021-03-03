b092291d9b64ae1672b862dab56891c28e1edf07
patryk <sdark431@gmail.com>
2021-03-03 01:26:25 +0100

    Naprawienie skoku

--- Assets/Scripte/Player.cs
+++ Assets/Scripte/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
@@ -13,7 +14,7 @@ public class Player : MonoBehaviour
     Rigidbody2D _rigidbody2D;
 
 
-    private Vector2 _move;
+    private float _move;
 
     // Start is called before the first frame update
     void Start()
@@ -27,28 +28,33 @@ public class Player : MonoBehaviour
     {
         ChangeAnimation();
         FlipSprite();
-        if (_move.y > 0) _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _move.y * jumpForce);
-        _rigidbody2D.velocity = new Vector3(_move.x * speed, _rigidbody2D.velocity.y);
-        
+        _rigidbody2D.velocity = new Vector3(_move * speed, _rigidbody2D.velocity.y);
+
     }
 
     private void ChangeAnimation()
     {
-        if (_move.x == 0) myAnimator.SetBool("isRunning", false);
+        if (_move == 0) myAnimator.SetBool("isRunning", false);
         else myAnimator.SetBool("isRunning", true);
     }
 
     private void FlipSprite()
     {
-        if (_move.x != 0)
+        if (_move != 0)
         {
-            transform.localScale = new Vector2(Mathf.Sign(_move.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(_move), 1f);
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        _move = context.ReadValue<Vector2>();
+        _move = context.ReadValue<float>();
+        Debug.Log(_move);
+    }
+    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _rigidbody2D.velocity += new Vector2(_rigidbody2D.velocity.x, context.ReadValue<float>() * jumpForce);
         Debug.Log(_move);
     }
     
