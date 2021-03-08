d41a7141547d57652b5e349e5ae0bfb8b0e59d86
patryk <sdark431@gmail.com>
2021-03-08 20:13:30 +0100

    Dodanie wyswietlania zycia w postaci serc

---
+++ Assets/Scripts/UI/HeartsHealthSystem.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartsHealthSystem
+{
+    public event EventHandler onDamaged;
+    public event EventHandler onHealed;
+    public event EventHandler onDead;
+
+    public const float MAX_HEARTH_VALUE = 1f;
+    private List<Heart> heartList;
+
+    public HeartsHealthSystem(int heartAmount)
+    {
+        heartList = new List<Heart>();
+        for (int i = 0; i < heartAmount; i++)
+        {
+            Heart heart = new Heart(1);
+            heartList.Add(heart);
+        }
+    }
+
+    public List<Heart> GetHeartList()
+    {
+        return heartList;
+    }
+
+    public void Damage(float damageAmount)
+    {
+        for (int i = heartList.Count -1; i >= 0; i--)
+        {
+            Heart heart = heartList[i];
+            if(damageAmount > heart.GetValue())
+            {
+                damageAmount -= heart.GetValue();
+                heart.TakeDamage(heart.GetValue());
+            } 
+            else
+            {
+                heart.TakeDamage(damageAmount);
+                break;
+            }
+        }
+
+        onDamaged?.Invoke(this, EventArgs.Empty);
+
+        if(IsDead()) onDead?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Heal(float healAmount)
+    {
+        for (int i = 0; i < heartList.Count; i++)
+        {
+            Heart heart = heartList[i];
+            float missingHP = MAX_HEARTH_VALUE - heart.GetValue();
+            if(healAmount > missingHP)
+            {
+                healAmount -= missingHP;
+                heart.Heal(missingHP);
+            }
+            else
+            {
+                heart.Heal(healAmount);
+                break;
+            }
+        }
+        onHealed?.Invoke(this, EventArgs.Empty);
+    }
+
+    public bool IsDead()
+    {
+        return heartList[0].GetValue() == 0;
+    }
+
+    public class Heart
+    {
+        private float value;
+
+        public Heart(float value)
+        {
+            this.value = value;
+        }
+
+        public float GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(float value)
+        {
+            this.value = value;
+        }
+
+        public void TakeDamage(float damageAmount)
+        {
+            if (damageAmount >= value)
+            {
+                value = 0;
+            }
+            else
+            {
+                value -= damageAmount;
+            }
+        }
+
+        public void Heal(float healAmount)
+        {
+            if(value + healAmount > MAX_HEARTH_VALUE)
+            {
+                value = MAX_HEARTH_VALUE;
+            }
+            else
+            {
+                value += healAmount;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HeartsHealthVisual.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey;
+
+public class HeartsHealthVisual : MonoBehaviour
+{
+    [SerializeField] GameObject heartObject;
+
+    private List<HeartImage> heartImageList;
+    private HeartsHealthSystem heartsHealthSystem;
+
+    private void Awake() {
+        heartImageList = new List<HeartImage>();
+    }
+
+    private void Start()
+    {
+        HeartsHealthSystem newHeartsHealthSystem = new HeartsHealthSystem(4);
+        SetHeartsHealthSystem(newHeartsHealthSystem);
+
+        heartsHealthSystem.Damage(1f);
+        heartsHealthSystem.Damage(1f);
+        heartsHealthSystem.Damage(1f);
+        heartsHealthSystem.Damage(1f);
+    }
+
+    public void SetHeartsHealthSystem(HeartsHealthSystem heartsHealthSystem)
+    {
+        this.heartsHealthSystem = heartsHealthSystem;
+
+        List<HeartsHealthSystem.Heart> heartList = heartsHealthSystem.GetHeartList();
+        Vector2 heartAnchorePosition = new Vector2(0,0);
+        for (int i = 0; i < heartList.Count; i++)
+        {
+            HeartsHealthSystem.Heart heart = heartList[i];
+            CreateHeartImage(heartAnchorePosition).SetHeartValue(heart.GetValue());
+            heartAnchorePosition += new Vector2(44,0);
+        }
+        heartsHealthSystem.onDamaged += RefreshAllHearts;
+        heartsHealthSystem.onHealed += RefreshAllHearts;
+        heartsHealthSystem.onDead += DeadMessage;
+    }
+
+    public void DeadMessage(object sender, EventArgs e)
+    {
+        Debug.Log("Dead");
+    }
+
+    public void RefreshAllHearts(object sender, EventArgs e)
+    {
+        for (int i = 0; i < heartImageList.Count; i++)
+        {
+            HeartImage heartImage =  heartImageList[i];
+            HeartsHealthSystem.Heart heart = heartsHealthSystem.GetHeartList()[i];
+            heartImage.SetHeartValue(heart.GetValue());
+        }
+    }
+
+    private HeartImage CreateHeartImage(Vector2 anchoredPosition)
+    {
+        GameObject heartGameObject = Instantiate(heartObject);
+        heartGameObject.transform.parent = transform;
+        heartGameObject.transform.localPosition = Vector3.zero;
+        heartGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(10,10); 
+        HeartImage heartImage = new HeartImage(heartGameObject);
+        heartImageList.Add(heartImage);
+
+        return heartImage;
+    }
+
+    public class HeartImage
+    { 
+        private GameObject herthImage;
+
+        public HeartImage(GameObject hearth)
+        {
+            this.herthImage = hearth;
+        }
+
+        public void SetHeartValue(float value)
+        {
+            herthImage.transform.Find("Fill").GetComponent<Image>().fillAmount = value * 0.8f;
+        }
+    }
+}
