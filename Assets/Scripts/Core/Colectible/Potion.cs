470ab30bc3eaedb21b2592d8ad7dbe5d9714198f
patryk <sdark431@gmail.com>
2021-03-09 01:07:31 +0100

    Dodanie potionow do zebrania i wyswietlanie ilosci posiadanych

---
+++ Assets/Scripts/Core/Colectible/Potion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    [SerializeField] PotionSystemVisual potionSystem;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player.PlayerCombat player = other.GetComponent<Player.PlayerCombat>();
+        if(player == null) return;
+        potionSystem.AddPotion();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/UI/PotionSystem/PotionSystemVisual.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionSystemVisual : MonoBehaviour
+{
+    [SerializeField] private Sprite potionSprite;
+
+    List<GameObject> potionList;
+
+    private const float OFFSET_POTION = 30f;
+
+    private void Awake() {
+        potionList = new List<GameObject>();
+    }
+
+    private GameObject CreatePotionImage(Vector2 anchoredPosition)
+    {
+        GameObject potionGameObject = new GameObject("Potion", typeof(Image));
+        potionGameObject.transform.parent = transform;
+        potionGameObject.GetComponent<Image>().sprite = potionSprite;
+        potionGameObject.transform.localPosition = Vector3.zero;
+
+        potionGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        potionGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(40,40);
+
+        potionList.Add(potionGameObject);
+        return potionGameObject;
+    }
+
+    public void UsePotion()
+    {
+        GameObject potion = potionList[potionList.Count -1];
+        potionList.Remove(potion);
+        Destroy(potion);
+    }
+
+    public void AddPotion()
+    {
+        var offset = potionList.Count * OFFSET_POTION;
+        CreatePotionImage(new Vector2(offset,0));
+    }
+
+}
