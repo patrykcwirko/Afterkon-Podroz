058f79d07d4814624c5a5f325f664f01410a9516
patryk <sdark431@gmail.com>
2021-03-11 12:30:36 +0100

    Naprawa z podpieciem AI do przeciwnika i animacja wysuwania sie platformy

--- Assets/Scripts/Core/Trap/MoveTrap.cs
+++ Assets/Scripts/Core/Trap/MoveTrap.cs
@@ -6,26 +6,28 @@ using UnityEngine;
 public class MoveTrap : MonoBehaviour
 {
     [SerializeField] private AnimationClip clip;
-    [SerializeField] private int tickBetweenChange = 20;
+    [SerializeField] private int maxTickBetweenChange = 30;
     [SerializeField] private int timeOff = 2;
 
     private int _lastTimeActivate;
+    private int _timeBetweenChange;
     private Animation _animation;
 
     void Awake()
     {
         _animation = GetComponent<Animation>();
-        _animation.AddClip(clip, "MoveTrap");
-        _animation.AddClip(clip, "MoveTrap");
+        _animation.AddClip(clip, clip.name);
+        _timeBetweenChange = UnityEngine.Random.Range(20, maxTickBetweenChange);
         TimeTickSystem.onTick += MoveOnTick;
     }
 
     private void MoveOnTick(object sender, TimeTickSystem.onTickEventArgs e)
     {
-        if (e.tick >= _lastTimeActivate + tickBetweenChange + timeOff)
+        if (e.tick >= _lastTimeActivate + _timeBetweenChange + timeOff)
         {
             _lastTimeActivate = e.tick;
-            _animation.Play("MoveTrap", PlayMode.StopAll);
+            _animation.Play(clip.name, PlayMode.StopAll);
+            _timeBetweenChange = UnityEngine.Random.Range(20, maxTickBetweenChange);
         }
     }
 }
