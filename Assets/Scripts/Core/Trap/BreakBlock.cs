c34a103b0ce0f15386e0a8b469491736f5327377
patryk <sdark431@gmail.com>
2021-03-16 23:37:03 +0100

    Dodanie niszczacysz sie blokow

---
+++ Assets/Scripts/Core/Trap/BreakBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlock : MonoBehaviour
+{
+    [SerializeField] Sprite[] breakSprite;
+
+    private int currentSpriteIndex = 0;
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if(currentSpriteIndex < breakSprite.Length)
+            {
+                Debug.Log(currentSpriteIndex);
+                transform.Find("BreakImage").GetComponent<SpriteRenderer>().sprite = breakSprite[currentSpriteIndex++];
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }    
+    }
+}
