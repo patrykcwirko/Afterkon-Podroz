9459203a31fe0ea866258e9f309637cbbf7bcb01
patrykcwirko <37484838+patrykcwirko@users.noreply.github.com>
2021-03-20 05:06:16 +0100

    Dodanie drzwi

---
+++ Assets/Scripts/Core/Door.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject key;
+
+    private SpriteRenderer _sprite;
+    private Color _defaultColor;
+
+    private void Start() 
+    {
+        _sprite = transform.GetComponent<SpriteRenderer>();
+        _defaultColor = _sprite.color;
+    }
+
+    void Update()
+    {
+        Open();
+    }
+
+    private void Open()
+    {
+        if(key.GetComponent<IKey>().CanOpen())
+        {
+            _sprite.color = new Color(_defaultColor.r, _defaultColor.g, _defaultColor.b, 0.25f);
+            transform.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            _sprite.color = _defaultColor;
+            transform.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
