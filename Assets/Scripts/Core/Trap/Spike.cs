0612405d6e92432001cab5ca3019fb178b0c92e0
patcwi <patcwirko@github.pl>
2021-04-03 15:12:50 +0200

    Dodanie poruszajacych sie kolcow

--- Assets/Scripts/Core/Trap/Spike.cs
+++ Assets/Scripts/Core/Trap/Spike.cs
@@ -9,6 +9,32 @@ namespace Core.Trap
         public float knockbackDuration = 0.2f;
         public float knockbackPower = 10f;
         public float damage = 10f;
+        public bool isMoving;
+        [SerializeField] private AnimationClip clip;
+        [SerializeField] private int tickBetweenChange = 30;
+        [SerializeField] private int timeOff = 2;
+
+        private int _lastTimeActivate;
+        private int _timeBetweenChange;
+        private Animation _animation;
+
+        void Awake()
+        {
+            _animation = GetComponent<Animation>();
+            if(isMoving) _animation.AddClip(clip, clip.name);
+            _timeBetweenChange = tickBetweenChange;
+            TimeTickSystem.onTick += MoveOnTick;
+        }
+
+        private void MoveOnTick(object sender, TimeTickSystem.onTickEventArgs e)
+        {
+            if (e.tick >= _lastTimeActivate + _timeBetweenChange + timeOff)
+            {
+                _lastTimeActivate = e.tick;
+                if(isMoving) _animation.Play(clip.name, PlayMode.StopAll);
+                _timeBetweenChange = tickBetweenChange;
+            }
+        }
 
         private void OnTriggerEnter2D(Collider2D other) 
         {
